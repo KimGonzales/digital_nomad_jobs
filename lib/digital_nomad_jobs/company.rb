c3c7b1336b973240b42eec2090fcfea35f3bb1f5
github username <github email address>
2018-03-03 19:22:32 -0500

    change indentation from 4 to 2 in company

--- lib/digital_nomad_jobs/company.rb
+++ lib/digital_nomad_jobs/company.rb
@@ -1,42 +1,42 @@
 class DigitalNomadJobs::Company
 
-    @@all = []
-    attr_accessor :name
-    attr_reader :jobs
+  @@all = []
+  attr_accessor :name
+  attr_reader :jobs
 
-    def initialize(name)
-        @name = name 
-        @jobs = []
-        @@all << self 
-    end 
+  def initialize(name)
+    @name = name 
+    @jobs = []
+    @@all << self 
+  end 
 
-    def self.find_by_name(name)
-        @@all.find {|company| company.name == name}
-    end 
+  def self.find_by_name(name)
+    @@all.find {|company| company.name == name}
+  end 
 
-    def self.create_by_name(name)
-        company = self.new(name)
-    end 
+  def self.create_by_name(name)
+    company = self.new(name)
+  end 
 
-    def self.find_or_create_by_name(name)
-        find_by_name(name) || create_by_name(name)
-    end
+  def self.find_or_create_by_name(name)
+    find_by_name(name) || create_by_name(name)
+  end
 
-    def self.all
-        @@all
-    end 
+  def self.all
+    @@all
+  end 
 
-    def self.reset
-        @@all.clear
-    end 
+  def self.reset
+    @@all.clear
+  end 
 
-    def add_job(job)
-        @jobs << job 
-        job.company = self
-    end 
+  def add_job(job)
+    @jobs << job 
+    job.company = self
+  end 
 
 
-    def print_jobs
-        jobs.each_with_index(1) {|job, i| puts "#{i}. #{job.title}"}
-    end 
+  def print_jobs
+    jobs.each_with_index(1) {|job, i| puts "#{i}. #{job.title}"}
+  end 
 end 
