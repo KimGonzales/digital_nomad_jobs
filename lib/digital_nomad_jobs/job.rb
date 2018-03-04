8dd304e8ec616833d570ece03fe56e0c2e501e1a
github username <github email address>
2018-03-03 19:23:33 -0500

    modified indentation in job.rb

--- lib/digital_nomad_jobs/job.rb
+++ lib/digital_nomad_jobs/job.rb
@@ -1,41 +1,41 @@
 class DigitalNomadJobs::Job
 
-    attr_accessor :title, :company, :description, :job_url, :skills, :time_posted, :company_url
-    @@all = [ ]
+  attr_accessor :title, :company, :description, :job_url, :skills, :time_posted, :company_url
+  @@all = [ ]
 
-    def initialize(attributes)
-        attributes.each {|key, value| self.send(("#{key}="), value)}
-        @@all << self 
-    end 
+  def initialize(attributes)
+    attributes.each {|key, value| self.send(("#{key}="), value)}
+    @@all << self 
+  end 
 
-    def self.create_from_collection(array)
-        array.each do | hash |
-            job = new(hash)
-        end 
+  def self.create_from_collection(array)
+    array.each do | hash |
+      job = new(hash)
     end 
+  end 
 
-    def add_job_description(hash)
-        hash.each {|key, value| self.send(("#{key}="), value)}
-    end 
+  def add_job_description(hash)
+    hash.each {|key, value| self.send(("#{key}="), value)}
+  end 
 
-    def self.all
-        @@all 
-    end 
+  def self.all
+    @@all 
+  end 
 
-    def self.reset
-        @@all.clear
-    end 
+  def self.reset
+    @@all.clear
+  end 
 
-    def print_description
-        puts "=================================================================================================================="
-        puts "Position: #{self.title}"
-        puts "Company : #{self.company}"
-        puts "Posted  : #{self.time_posted} ago"
-        puts "Tags : #{self.skills}"
-        puts ""
-        puts "Description : #{self.description}"
-        puts ""
-        puts "Learn More or Apply at: #{'https://remoteok.io' + self.job_url}"
-        puts "=================================================================================================================="
-    end 
+  def print_description
+    puts "=================================================================================================================="
+    puts "Position: #{self.title}"
+    puts "Company : #{self.company}"
+    puts "Posted  : #{self.time_posted} ago"
+    puts "Tags : #{self.skills}"
+    puts ""
+    puts "Description : #{self.description}"
+    puts ""
+    puts "Learn More or Apply at: #{'https://remoteok.io' + self.job_url}"
+    puts "=================================================================================================================="
+  end 
 end
