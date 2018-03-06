2d271c4f9f9951c27896e26ce36e177d653da2ed
github username <github email address>
2018-03-06 10:19:13 -0500

    modify specs

--- spec/digital_nomad_jobs_spec.rb
+++ spec/digital_nomad_jobs_spec.rb
@@ -2,9 +2,5 @@ RSpec.describe DigitalNomadJobs do
   it "has a version number" do
     expect(DigitalNomadJobs::VERSION).not_to be nil
   end
-
-  it "does something useful" do
-    expect(false).to eq(true)
-  end
 end
 
